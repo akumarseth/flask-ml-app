492e5848f8a18896301910aa72fe0f99bd09434f
Abhishek Kumar <Abhishek.Kumar13@gds.ey.com>
2021-09-09 23:45:20 +0530

    updated configuration

--- project/server/config.py
+++ project/server/config.py
@@ -20,6 +20,11 @@ class BaseConfig:
     BCRYPT_LOG_ROUNDS = 13
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    STORAGE_ACCOUNT_NAME = 'dssstorageflaskapp'
+    CONTAINER_NAME = 'input'
+    CONFIG_CONTAINER_NAME='config'
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
+    MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
     
 
 
@@ -32,14 +37,9 @@ class DevelopmentConfig(BaseConfig):
     # STORAGE_ACCOUNT_NAME = 'pythonstorageaccount01'
     # ACCOUNT_KEY = 'KJ7dCf+yykXGmotjYtKgwgYjcm4BWlC56TajsfShAhu0UnYHhOPYJzmKo/4r6lfAXpCQ6o5aOvhKmp7kprBT7g=='
     # CONNECTION_STRING = f"DefaultEndpointsProtocol=https;AccountName={STORAGE_ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net"
-    CONFIG_CONTAINER_NAME='config'
-    CONTAINER_NAME = 'input'
-    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
-    MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
-    
-    STORAGE_ACCOUNT_NAME = 'dssstorageflaskapp'
+        
     ACCOUNT_KEY = 'pdLARQZbo5mjcJkKnWVLbGBVdhFdr9/hORs9AxoG4skXOWxpnp+HEb81hkiCFovEQDKvRI9jEa44ghVN2aMklQ=='
-    CONNECTION_STRING = f"DefaultEndpointsProtocol=https;AccountName={STORAGE_ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net"
+    CONNECTION_STRING = f"DefaultEndpointsProtocol=https;AccountName={BaseConfig.STORAGE_ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net"
 
 
 class TestingConfig(BaseConfig):
@@ -49,12 +49,10 @@ class TestingConfig(BaseConfig):
     BCRYPT_LOG_ROUNDS = 4
     SQLALCHEMY_DATABASE_URI = "postgresql://qywtuaouswpwun:90eacb2e62e6fea6b00a33a0c7dbaabcc552292b789e9b840948aa8ceaa052e5@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d4986ivbgijcja"
     PRESERVE_CONTEXT_ON_EXCEPTION = False
-    STORAGE_ACCOUNT_NAME = 'dssstorageflaskapp'
+    
     ACCOUNT_KEY = 'pdLARQZbo5mjcJkKnWVLbGBVdhFdr9/hORs9AxoG4skXOWxpnp+HEb81hkiCFovEQDKvRI9jEa44ghVN2aMklQ=='
-    CONNECTION_STRING = f"DefaultEndpointsProtocol=https;AccountName={STORAGE_ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net"
-    CONTAINER_NAME = 'input'
-    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg'])
-    MAX_CONTENT_LENGTH = 20 * 1024 * 1024    # 20 Mb limit
+    CONNECTION_STRING = f"DefaultEndpointsProtocol=https;AccountName={BaseConfig.STORAGE_ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net"
+    
 
 
 class ProductionConfig(BaseConfig):
